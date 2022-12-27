f7984d008a2057aa3ea1d95e1651088d1641eb66
justtuananh <tuananh.nk.18@gmail.com>
2022-12-27 08:07:11 +0700

    cập nhật trang giỏ hàng

---
+++ testASPnet/Controllers/giohangController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testASPnet.Controllers
+{
+    public class giohangController : Controller
+    {
+        // GET: giohang
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
